b94dbf47e5014e54b82b56f6fd39b0164c184bfe
LujieDuan <66813185@qq.com>
2018-05-14 17:03:58 -0600

    Fixed csv by using a csv writter

--- CincinnatiProject.py
+++ CincinnatiProject.py
@@ -1,35 +1,50 @@
 # -*- coding: utf-8 -*-
-import sys,getopt,datetime,codecs
+import sys,csv,codecs
 if sys.version_info[0] < 3:
     import got
 else:
     import got3 as got
 
-def main():
+def main(search_keyword):
 	try:
 		tweetCriteria = got.manager.TweetCriteria()
-		outputFileName = "cincinnati_all_output_got.csv"
+		outputFileName = "cincinnati_tweets_%s.csv" % search_keyword
 
-		tweetCriteria.maxTweets = int(10000000)
+		tweetCriteria.maxTweets = int(10000000000)
 		tweetCriteria.near = "Cincinnati"
 		tweetCriteria.within = "15mi"
-				
-		outputFile = codecs.open(outputFileName, "w+", "utf-8")
+		tweetCriteria.querySearch = '"' + search_keyword + '"'
 
-		outputFile.write('username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink')
+		csvfile = open(outputFileName, "w+", encoding="utf-8")
+		csvwriter = csv.writer(csvfile)
+
+		csvwriter.writerow(['username','date','retweets','favorites','text','geo','mentions','hashtags','id','permalink'])
 
 		print('Searching...\n')
 
 		def receiveBuffer(tweets):
 			for t in tweets:
-				outputFile.write(('\n%s;%s;%d;%d;"%s";%s;%s;%s;"%s";%s' % (t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.geo, t.mentions, t.hashtags, t.id, t.permalink)))
-			outputFile.flush()
+				csvwriter.writerow([t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets,t.favorites, t.text, t.geo, t.mentions, t.hashtags, t.id, t.permalink])
+			csvfile.flush()
 			print('More %d saved on file...\n' % len(tweets))
 
 		got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
 	finally:
-		outputFile.close()
+		csvfile.close()
 		print('Done. Output file generated "%s".' % outputFileName)
 
 if __name__ == '__main__':
-	main()
+
+
+	# keywords = ['opioid', 'heroin']
+	# keywords = ['fentanyl', 'darkweb', 'dark web', 'tor', 'naloxone', 'detox', 'detoxification', 'drug']
+
+	keywords = ['overdose']
+
+	argv = sys.argv[1:]
+
+	if len(argv) == 0:
+		for k in keywords:
+			main(k)
+	else:
+		main(argv[0])
